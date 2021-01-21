d4573fea0fc33666b207f04106d877b03eb16ea4
Filipe Gomes <fgdl.py91@gmail.com>
2021-01-21 19:38:53 -0300

    Update database and modules

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common'
 import { ProductService } from './product.service'
-
+import { Product } from './product.entity'
+import { promises } from 'fs'
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
   @Get()
-  async findAll(): Promise<any> {
+  async findAll(): Promise<Product[]> {
     return this.productService.findAll()
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<Product> {
+    return this.productService.findById(id)
+  }
+  @Post()
+  async create(@Body() product: Product): Promise<Product> {
+    return this.productService.create(product)
+  }
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<boolean> {
+    return this.productService.remove(id)
   }
 }
